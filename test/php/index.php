0539000bbcf9d17a35c3bee8bf4d8b59020b6d67
Dimitris <jimkakour@gmail.com>
2017-05-17 23:32:13 +0300

    Add files via upload

--- test/php/index.php
+++ test/php/index.php
@@ -3,7 +3,23 @@
 </head>
 
 <body>
-<?php
-echo "giannis"; ?>
+
+<table align="center" id="logintable">
+  <tr>
+  <td><label for= "username">Username</label>
+  </td>
+  <td>
+    <input type="text" id="username" name="username">
+  </td>
+  </tr>
+  <tr>
+  <td><label for= "username">Password</label>
+
+  <td>
+    <input type="text" id="password" name="password">
+  </td>
+    </tr>
+</table>
+
 
 </body>
