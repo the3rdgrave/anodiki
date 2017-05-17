6f69f3eca62f8d83799e1ed554d819f1bee06b93
elgrecoo <dexterakos5@yahoo.gr>
2017-05-18 00:54:16 +0300

    Add files via upload

---
+++ myproj/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+<head>
+
+  <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+  <link href="style/style.css" rel="stylesheet">
+</head>
+  <body>
+    <form method="post" action="mainpage.php">
+    <table id="logintable" align="center">
+      <tr>
+        <td><label for="username">ΟΝΟΜΑ ΧΡΗΣΤΗ</label>
+        </td>
+        <td>
+          <input type="text" id="username" name="username">
+        </td>
+      </tr>
+      <tr>
+        <td><label for="password">ΚΩΔΙΚΟΣ</label>
+        </td>
+        <td>
+          <input type="text" id="password" name="password">
+        </td>
+      </tr>
+      <tr>
+        <td colspan="2" style="text-align: center">
+          <button type="submit0">Είσοδος</button>
+        </td>
+      </tr>
+    </table>
+  </form>
+</body>
+
+</html>
---
+++ myproj/mainpage.php
@@ -0,0 +1,82 @@
+<table id="formtable" align="center" frame="void" border="2px solid black">
+  <tr>
+  <td colspan="3" style="text-align:center">
+<p>
+  ΕΙΣΑΓΩΓΗ ΣΤΟΙΧΕΙΩΝ ΞΕΝΟΔΟΧΕΙΟΥ
+</p>
+  </td>
+</tr>
+    <tr>
+      <td>
+      <label for="name">ΟΝΟΜΑ</label>
+    </td>
+    <td>
+      <input type="text" id="name" name="name">
+    </td>
+    </tr>
+    <tr>
+      <td>
+      <label for="address">ΔΙΕΥΘΥΝΣΗ</label>
+    </td>
+    <td>
+      <input type="text" id="address" name="address">
+    </td>
+    </tr>
+    <tr>
+      <td>
+      <label for="maintainer">ΣΥΝΤΗΡΗΤΗΣ</label>
+    </td>
+    <td>
+      <input type="text" id="mainname" name="mainname">
+    </td>
+    <td>
+      <input type="text" id="mainlast" name="mainlast">
+    </td>
+    </tr>
+    <tr>
+      <td>
+      <label for="phone1">ΤΗΛΕΦΩΝΟ</label>
+    </td>
+    <td>
+      <input type="text" id="phone1" name="phone1">
+    </td>
+    <td>
+      <input type="text" id="phone2" name="phone2">
+    </td>
+    </tr>
+    <tr>
+      <td>
+      <label for="emailreport">EMAIL REPORT</label>
+    </td>
+    <td>
+      <input type="text" id="emailreport" name="emailreport">
+    </td>
+    <td>
+      <input type="text" id="emailreport2" name="emailreport2">
+    </td>
+    </tr>
+  <tr>
+    <td colspan="3" style="text-align:center">
+      <p>ΕΙΣΑΓΩΓΗ ΣΤΟΙΧΕΙΩΝ ΣΥΝΤΗΡΗΣΗΣ</p>
+    </td>
+  </tr>
+  <tr>
+    <td colspan="3" style="text-align:center">
+      <p>ΧΩΡΟΣ</p>
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <input type="text" id="room" name="room">
+    </td>
+    <td>
+    <p>ΕΡΓΑΣΙΕΣ</p>
+    </td>
+    <td>
+    <p>ΗΜΕΡΕΣ ΜΕΧΡΙ ΤΟΝ<br> ΕΠΟΜΕΝΟ ΕΛΕΓΧΟ</p>
+    </td>
+
+  </tr>
+
+
+</table>
